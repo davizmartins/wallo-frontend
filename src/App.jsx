27d0b3d3cc987581090442f8d6792d1b671095b3
Davi Martins <davi.gurgel@sga.pucminas.br>
2026-08-18 20:03:47 -0300

    feat: configura rotas com layout e protecao

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Layout from "./components/Layout";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+      {/* Rota pública: login */}
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+
+      {/* Rotas internas: protegidas e dentro do Layout (com a sidebar) */}
+      <Route
+        element={
+          <PrivateRoute>
+            <Layout />
+          </PrivateRoute>
+        }
+      >
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* As próximas telas entram aqui, uma por linha */}
+      </Route>
+
+      {/* Raiz redireciona pro dashboard */}
+      <Route path="/" element={<Navigate to="/dashboard" />} />
     </Routes>
   );
 }
