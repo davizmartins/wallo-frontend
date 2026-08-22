55109b1545091f9d7fa73d5ea2970ff75fe28868
Davi Martins <davi.gurgel@sga.pucminas.br>
2026-08-22 13:23:58 -0300

    refactor: centraliza rotas em arquivo dedicado

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Categories from "./pages/Categories/Categories";
-import Layout from "./components/Layout";
-import PrivateRoute from "./components/PrivateRoute";
+import Layout from "./components/Layout/Layout";
+import Accounts from "./pages/Accounts/Accounts";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Transactions from "./pages/Transactions/Transactions";
+import { routes } from "./routes";
 
 function App() {
   return (
     <Routes>
       {/* Rota pública: login */}
-      <Route path="/login" element={<Login />} />
+      <Route path={routes.login} element={<Login />} />
 
       {/* Rotas internas: protegidas e dentro do Layout (com a sidebar) */}
       <Route
@@ -19,13 +22,15 @@ function App() {
           </PrivateRoute>
         }
       >
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/categories" element={<Categories />} />
+        <Route path={routes.dashboard} element={<Dashboard />} />
+        <Route path={routes.categories} element={<Categories />} />
+        <Route path={routes.accounts} element={<Accounts />} />
+        <Route path={routes.transactions} element={<Transactions />} />
         {/* As próximas telas entram aqui, uma por linha */}
       </Route>
 
       {/* Raiz redireciona pro dashboard */}
-      <Route path="/" element={<Navigate to="/dashboard" />} />
+      <Route path="/" element={<Navigate to={routes.dashboard} />} />
     </Routes>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+export const routes = {
+  login: "/login",
+  dashboard: "/dashboard",
+  categories: "/categories",
+  accounts: "/accounts",
+  transactions: "/transactions",
+};
